cb25726fe379a30dff909f2ce618b0a00c74956b
Leonardo Faria <leonardofaria@gmail.com>
2019-09-21 15:52:58 -0700

    Make modal accessible

--- app.js
+++ app.js
@@ -1,42 +1,8 @@
-const body = document.body;
-const modal = document.querySelector('.modal');
+const navDialogEl = document.querySelector('.dialog');
+const dialogOverlay = document.querySelector('.dialog-overlay');
 
-const openModal = document.querySelectorAll('.modal-open');
-const closeModal = document.querySelectorAll('.modal-close');
-const overlay = document.querySelector('.modal-overlay');
-
-const toggleModal = function() {
-  modal.classList.toggle('opacity-0');
-  modal.classList.toggle('pointer-events-none');
-  body.classList.toggle('modal-active');
-};
-
-for (let i = 0; i < openModal.length; i++) {
-  openModal[i].addEventListener('click', function(event) {
-    event.preventDefault();
-    toggleModal();
-  });
-};
-
-for (let i = 0; i < closeModal.length; i++) {
-  closeModal[i].addEventListener('click', toggleModal);
-}
-
-overlay.addEventListener('click', toggleModal);
-
-document.onkeydown = function(evt) {
-  evt = evt || window.event;
-  let isEscape = false;
-
-  if ("key" in evt) {
-    isEscape = (evt.key === "Escape" || evt.key === "Esc");
-  } else {
-    isEscape = (evt.keyCode === 27);
-  }
-  if (isEscape && document.body.classList.contains('modal-active')) {
-    toggleModal();
-  }
-};
+const myDialog = new Dialog(navDialogEl, dialogOverlay);
+myDialog.addEventListeners('.open-dialog', '.close-dialog');
 
 
 const menuButton = document.getElementById('menu-button');
