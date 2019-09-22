7f22f8b5f1e2219784c14bb9ae5a5dd1f6a081f5
Leonardo Faria <leonardofaria@gmail.com>
2019-09-22 14:52:30 -0700

    Implement permalinks

--- app.js
+++ app.js
@@ -59,33 +59,51 @@ const collectionLabel = document.getElementById('collection-label');
 const uiButtons = document.querySelectorAll('.computers-button, .idevices-button');
 const modeButtons = document.querySelectorAll('.mode-button');
 
-const toggleContainers = function() {
-  idevicesContainer.classList.toggle('hidden');
-  computersContainer.classList.toggle('hidden');
+const showCollection = function(collection) {
+  let items = null;
+  let label = null;
+
+  if (collection === 'computers') {
+    items = document.querySelectorAll('#computers-container .card');
+    label = 'Computers (' + items.length + ')';
+    idevicesContainer.classList.add('hidden');
+    computersContainer.classList.remove('hidden');
+  } else {
+    items = document.querySelectorAll('#idevices-container .card');
+    label = 'iDevices (' + items.length + ')';
+    idevicesContainer.classList.remove('hidden');
+    computersContainer.classList.add('hidden');
+  }
 
   uiButtons.forEach(function(element) {
-    element.classList.toggle('text-gray-800');
-    element.classList.toggle('text-purple-600');
+    const dataCollection = element.dataset.collection;
+    if (dataCollection === collection) {
+      element.classList.remove('text-gray-800');
+      element.classList.add('text-purple-600');
+    } else {
+      element.classList.add('text-gray-800');
+      element.classList.remove('text-purple-600');
+    }
   });
 
+  collectionLabel.innerHTML = label;
+
+  const params = new URLSearchParams(location.search);
+  params.set('collection', collection);
+  window.history.replaceState({}, '', `${location.pathname}?${params}`);
+  // TODO: replaceState is not updating the title
+  document.title = `${label} - Leo's Collection`;
+
+  // Mobile: close menu after click
   if (!menuContainer.classList.contains('hidden')) {
     menuContainer.classList.add('hidden');
   }
 }
 
-computersButton.forEach(function(element) {
+uiButtons.forEach(function(element) {
   element.addEventListener('click', function(event) {
-    const items = document.querySelectorAll('#computers-container .card');
-    collectionLabel.innerHTML = 'Computers (' + items.length + ')';
-    toggleContainers();
-  });
-});
-
-idevicesButton.forEach(function(element) {
-  element.addEventListener('click', function(event) {
-    const items = document.querySelectorAll('#idevices-container .card');
-    collectionLabel.innerHTML = 'iDevices (' + items.length + ')';
-    toggleContainers();
+    const collection = element.dataset.collection;
+    showCollection(collection);
   });
 });
 
@@ -101,16 +119,37 @@ const toggleMode = function(mode) {
     computersGrid.classList.add('hidden');
     idevicesGrid.classList.add('hidden');
   }
+
+  modeButtons.forEach(function(button) {
+    const dataMode = button.dataset.mode;
+    if (dataMode === mode) {
+      button.classList.remove('text-gray-800');
+      button.classList.add('text-purple-600');
+    } else {
+      button.classList.add('text-gray-800');
+      button.classList.remove('text-purple-600');
+    }
+  });
+
+  const params = new URLSearchParams(location.search);
+  params.set('mode', mode);
+  window.history.replaceState({}, '', `${location.pathname}?${params}`);
 }
 
 modeButtons.forEach(function(element) {
   element.addEventListener('click', function(event) {
     const mode = element.dataset.mode;
     toggleMode(mode);
-
-    modeButtons.forEach(function(button) {
-      button.classList.toggle('text-gray-800');
-      button.classList.toggle('text-purple-600');
-    });
   });
 });
+
+
+const params = new URLSearchParams(location.search);
+
+if (params.get('collection') !== '') {
+  showCollection(params.get('collection'));
+}
+
+if (params.get('mode') !== '') {
+  toggleMode(params.get('mode'));
+}
